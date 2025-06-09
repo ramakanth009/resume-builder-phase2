40c7e019f38f322b83d52c25424e9a1646d01122
Your Name <you@example.com>
2025-06-09 11:56:58 +0530

    latest

--- src/templates/pdf/ClassicPDFTemplate.jsx
+++ src/templates/pdf/ClassicPDFTemplate.jsx
@@ -10,7 +10,7 @@ const styles = StyleSheet.create({
   name: {
     fontSize: 24,
     fontWeight: "bold",
-    marginBottom: 10,
+    marginBottom: 20,
   },
   contactInfo: {
     flexDirection: "row",
